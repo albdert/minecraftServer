65c84dd92c83789b113f8ce75e9b66d7f1a714ac
albdert <98839427+albdert@users.noreply.github.com>
2025-01-13 00:02:09 +0100

    Delete nettside directory
    rydde opp ...

--- nettside/script.js
+++
@@ -1,9 +0,0 @@
-try{
-    const res = await fetch('https://api.mcsrvstat.us/3/158.248.114.241:25565');
-    const data = await res.json();
-    console.log(data);
-
-}
-catch(error){
-    console.error(error);
-}
